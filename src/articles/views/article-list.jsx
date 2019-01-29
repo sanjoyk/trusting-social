21213a29228fcf64049b0c9821afd967bae03332
sanjoyk <sanjoyksardar01@gmail.com>
2019-01-29 17:31:50 +0530

    add lazy loaded

--- src/articles/views/article-list.jsx
+++ src/articles/views/article-list.jsx
@@ -5,6 +5,10 @@ import debounce from "lodash.debounce";
 import GridItem from '../../app-components/grid-item';
 import { withStyles } from '@material-ui/core/styles';
 
+import { LazyLoadImage, trackWindowScroll } from 'react-lazy-load-image-component';
+import AppLoader from '../../app-components/app-loader';
+
+
 const styles = theme => ({
   root: {
     display: 'flex',
@@ -63,7 +67,7 @@ class ArticleList extends PureComponent {
   }
 
   render() {
-    const { articlesByIds, classes } = this.props;
+    const { articlesByIds, classes, scrollPosition } = this.props;
     return (
       <div id="artical-list-container">
         <GridContiner className={classes.root}  >
@@ -72,9 +76,17 @@ class ArticleList extends PureComponent {
               return (
                 <GridItem style={{ display: "inline-block", boxSizing: "border-box" }} xs={4} sm={4} md={4} key={key}>
                   <div className={classes.articleContainer}>
-                    <img style={{ width: "100%" }}
+                    <LazyLoadImage
                       src={this.getImageUrl(multimedia)}
+                      effect="blur"
                       alt=""
+                      width="100%"
+                      scrollPosition={scrollPosition}
+                      beforeLoad={() => { console.log("laoding before") }}
+                      afterLoad={() => console.log("After load")}
+                      delayTime={10000}
+                      threshold={-100}
+                      placeholder={<AppLoader />}
                     />
                     <p style={{
                       textTransform: "uppercase",
@@ -86,7 +98,9 @@ class ArticleList extends PureComponent {
                       lineHeight: "1.17",
                       letterSpacing: "normal",
                       textAlign: "left",
-                    }}>{`${source}/${pubDate}`}</p>
+                    }}>
+                      {`${source}/${pubDate}`}
+                    </p>
 
                     <h2 style={{
                       paddingTop: "10px",
@@ -96,7 +110,9 @@ class ArticleList extends PureComponent {
                       lineHeight: "1.4",
                       textAlign: "left",
                       color: "#010001",
-                    }}>{snippet}</h2>
+                    }}>
+                      {snippet}
+                    </h2>
                   </div>
                 </GridItem>
               )
@@ -111,4 +127,4 @@ class ArticleList extends PureComponent {
   }
 }
 
-export default withStyles(styles)(ArticleList);;
+export default withStyles(styles)(trackWindowScroll(ArticleList));
